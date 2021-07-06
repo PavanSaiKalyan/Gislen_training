b8585ed4e0a7b60c07f0944e52799ecf12bf531f
Pavan Sai Kalyan  Yeluri <83338332+PavanSaiKalyan@users.noreply.github.com>
2021-07-06 11:59:15 +0530

    First day assignments

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyFirstProgram {
+    class Hello{
+        static void Main(string[] args){
+            Console.WriteLine("Enter your name:");
+            string myName = Console.ReadLine();
+            Console.WriteLine("The name is " +myName);
+
+
+        }
+    }
+}
---
+++ sumof/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sumof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,sum;
+            Console.WriteLine("Enter the first number:");
+            num1 = int.Parse( Console.ReadLine());
+            Console.WriteLine("Enter the second number:"); 
+            num2 = int.Parse(Console.ReadLine());
+            sum = num1+num2;
+            Console.WriteLine(sum);
+
+        }
+    }
+}
